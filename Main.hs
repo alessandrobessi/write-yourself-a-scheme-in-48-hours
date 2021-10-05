3f68fe8e458008aa7c37d36ce43383b0096cb978
Alessandro Bessi <alessandro.bessi@mail.com>
2021-10-05 17:05:57 +0200

    add evaluator primitives

--- Main.hs
+++ Main.hs
@@ -75,12 +75,16 @@ showVal (DottedList head tail) = "(" ++ unwordsList head ++ " . " ++ showVal tai
 
 instance Show LispVal where show = showVal
 
-readExpr :: String -> String
+eval :: LispVal -> LispVal
+eval val@(String _) = val
+eval val@(Number _) = val
+eval val@(Bool _) = val
+eval (List [Atom "quote", val]) = val
+
+readExpr :: String -> LispVal
 readExpr input = case parse parseExpr "lisp" input of 
-    Left err -> "No match: " ++ show err
-    Right val -> "Found " ++ show val
+    Left err -> String $ "No match: " ++ show err
+    Right val -> val
 
 main :: IO ()
-main = do
-    (expr:_) <- getArgs
-    putStrLn $ readExpr expr
+main = getArgs >>= print . eval . readExpr . head
